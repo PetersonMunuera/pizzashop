67c93ddc2ba42f1b12b79e8ba30c180877ef5df0
Peterson Munuera <peterson.munuera@gmail.com>
2024-06-28 18:31:47 -0300

    feat: handle pagination

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,5 +1,8 @@
 import { api } from '@/lib/axios'
 
+interface GetOrderQuery {
+  pageIndex: number | null
+}
 interface GetOrdersResponse {
   orders: {
     orderId: string
@@ -15,10 +18,10 @@ interface GetOrdersResponse {
   }
 }
 
-export async function getOrders() {
+export async function getOrders({ pageIndex }: GetOrderQuery) {
   const response = await api.get<GetOrdersResponse>('/orders', {
     params: {
-      pageIndex: 0,
+      pageIndex,
     },
   })
 
