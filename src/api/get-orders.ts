549fc45d9d4fce052f25b3b9daecd7a563735fa1
Peterson Munuera <peterson.munuera@gmail.com>
2024-06-28 19:05:37 -0300

    feat: handle filters

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -2,6 +2,9 @@ import { api } from '@/lib/axios'
 
 interface GetOrderQuery {
   pageIndex: number | null
+  orderId: string | null
+  customerName: string | null
+  status: string | null
 }
 interface GetOrdersResponse {
   orders: {
@@ -18,10 +21,18 @@ interface GetOrdersResponse {
   }
 }
 
-export async function getOrders({ pageIndex }: GetOrderQuery) {
+export async function getOrders({
+  pageIndex,
+  customerName,
+  orderId,
+  status,
+}: GetOrderQuery) {
   const response = await api.get<GetOrdersResponse>('/orders', {
     params: {
       pageIndex,
+      orderId,
+      customerName,
+      status,
     },
   })
 
